770cfd6582c5c1bf94b007df232916380253e6b8
Student <example@example.com>
2019-10-25 09:24:17 -0700

    add unworking branching in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
 
   $("#mainform").submit(function(event) {
 
+    if (portland && green && fall && dog) {
+      $(".jsanswer").show();
+    } else if (red && summer || outside) {
+      $(".rubyanswer").show();
+    } else if (snake || programmer || blue || spring) {
+      $(".pythonanswer").show();
+    } else {
+      $(".rustanswer").show();
+    }
+
 
     console.log("it submitted.");
     event.preventDefault();
